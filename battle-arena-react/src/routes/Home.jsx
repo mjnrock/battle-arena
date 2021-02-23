4f1d647195b4e34e1750e67fe5451e2c7515f0db
Matt Nawrocki <mjnrock@umich.edu>
2021-02-22 20:02:24 -0500

    init

--- battle-arena-react/src/routes/Home.jsx
+++ battle-arena-react/src/routes/Home.jsx
@@ -3,7 +3,18 @@ import React from "react";
 
 import GameView from "../components/GameView";
 
+import Choice from "../lib/Choice";
+
 export default function Home() {
+    let choice = new Choice(() => false);
+    let subchoice = new Choice(() => true);
+    choice.T(() => 5);
+    choice.F(subchoice);
+    subchoice.T(() => true);
+    subchoice.F(() => false);
+
+    console.log(choice.run());
+
     return (
         <GameView />
     )
