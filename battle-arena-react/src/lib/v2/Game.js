c9dc9696da3c6382a079f6d9c8c092a7d8b14aae
Matt Nawrocki <mjnrock@umich.edu>
2021-02-26 16:24:38 -0500

    init

--- battle-arena-react/src/lib/v2/Game.js
+++ battle-arena-react/src/lib/v2/Game.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 import Agency from "@lespantsfancy/agency";
 
-import GameLoop from "./GameLoop";
 import TileCanvas from "./TileCanvas";
 
 export default class Game extends Agency.Observable {
@@ -10,17 +9,16 @@ export default class Game extends Agency.Observable {
         
         this._lastUpdate = 0;
         
-        this.loop = new GameLoop(fps, this.onTick);
+        this.loop = Agency.Pulse.Generate(fps, { autostart: false }, ({ now }) => {
+            console.log(now);
+            this._lastUpdate = now;
+        });
         this.canvas = new TileCanvas(25, 25, { width: 1000, height: 800, props: { fillStyle: "rgba(0, 0, 255, 1.0)", strokeStyle: "#000" } });
 
         // Create Singleton pattern
         if(!Game.Instance) {
             Game.Instance = this;
         }
-    }
-
-    onTick(dt, now) {
-        this._lastUpdate = now;
     }
 
     // Access Singleton pattern via Game.$
@@ -37,7 +35,7 @@ export default class Game extends Agency.Observable {
                     1, 1, { isFilled: true }
                 );
             }
-            Game.$.loop.start();
+            Game.$.loop.subject.start();
         }
 
         return Game.Instance;
