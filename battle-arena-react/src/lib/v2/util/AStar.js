dc9d198572a67e0b4046b48544624abe35924173
Matt Nawrocki <mjnrock@umich.edu>
2021-03-10 12:18:09 -0500

    init

--- battle-arena-react/src/lib/v2/util/AStar.js
+++ battle-arena-react/src/lib/v2/util/AStar.js
@@ -1,13 +1,5 @@
-export function heuristic(a, b) {
-    const [ x1, y1 ] = a;
-    const [ x2, y2 ] = b;
-
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-    // return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
-}
-
-export function biasedHeuristic(p0, p1, p2, isBiased) {
-    if (isBiased && (p2.length > 0)) {
+export function heuristic(p0, p1, p2, isBiased) {
+    if (isBiased && p2.length) {
         return (Math.abs(p0[ 0 ] - p1[ 0 ]) + Math.abs(p0[ 1 ] - p1[ 1 ])) * (Math.abs((p0[ 0 ] - p1[ 0 ]) * (p2[ 1 ] - p1[ 1 ]) - (p2[ 0 ] - p1[ 0 ]) * (p0[ 1 ] - p1[ 1 ])) * 0.001);
     }
 
@@ -34,7 +26,7 @@ export function findPath(world, start = [], goal = []) {
                 current = came_from[ current.toString() ];
             }
 
-            return path.reverse();
+            return path.reverse();  // This starts with the goal and works backward, thus reverse the array so i = 0 is the start
         }
 
         for (let next of world.adjacent(...current, false)) {
@@ -42,7 +34,7 @@ export function findPath(world, start = [], goal = []) {
 
             if (!(next.toString() in cost_so_far) || (new_cost < cost_so_far[ next.toString() ])) {
                 cost_so_far[ next.toString() ] = new_cost;
-                let priority = new_cost + biasedHeuristic(next, goal, start, true);
+                let priority = new_cost + heuristic(next, goal, start, true);
 
                 frontier.push([ next, priority ]);
 
