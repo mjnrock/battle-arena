ec3d76f06f0ae0ef5ce3b753da013fad7e58e848
Matt Nawrocki <mjnrock@umich.edu>
2021-03-16 15:25:25 -0400

    init

--- battle-arena-react/src/lib/v2/data/entity/ai/random.js
+++ battle-arena-react/src/lib/v2/data/entity/ai/random.js
@@ -2,7 +2,7 @@ import Agency from "@lespantsfancy/agency";
 
 export function random(entity) {    
     const random = () => Math.round(parseFloat((Agency.Util.Dice.random(-5, 5) / 5)));
-            
+    
     entity.position.x = Math.max(0, Math.min(19, entity.position.x + random()));
     entity.position.y = Math.max(0, Math.min(19, entity.position.y + random()));
     entity.position.facing = Math.floor(Math.random() * 4) * 90;
