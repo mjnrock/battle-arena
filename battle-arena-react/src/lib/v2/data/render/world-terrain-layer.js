3a4161b83ab89e9287e199d37a5634e56d85d9db
Matt Nawrocki <mjnrock@umich.edu>
2021-03-16 11:55:07 -0400

    init

--- battle-arena-react/src/lib/v2/data/render/world-terrain-layer.js
+++ battle-arena-react/src/lib/v2/data/render/world-terrain-layer.js
@@ -3,7 +3,7 @@ import { DictTerrain } from "../entity/components/terrain";
 
 export function comparator(data = {}, oldData = {}) {
     return data.hash !== oldData.hash;
-    return !(data.x === oldData.x && data.y === oldData.y);
+    // return !(data.x === oldData.x && data.y === oldData.y);
 }
 
 export async function drawLayer(dt, elapsed, terrain) {            
