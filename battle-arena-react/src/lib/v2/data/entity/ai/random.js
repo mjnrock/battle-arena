e1175dcc3e2c9b07f8ee82748a65fdb6101e8f7d
Matt Nawrocki <mjnrock@umich.edu>
2021-03-21 18:24:14 -0400

    init

--- battle-arena-react/src/lib/v2/data/entity/ai/random.js
+++ battle-arena-react/src/lib/v2/data/entity/ai/random.js
@@ -4,9 +4,9 @@ export function random(entity) {
     const random = () => Math.round(parseFloat((Agency.Util.Dice.random(-5, 5) / 5)));
     
     if(Agency.Util.Dice.coin()) {
-        // entity.position.x = Math.max(0, Math.min(19, entity.position.x + random()));
-        // entity.position.y = Math.max(0, Math.min(19, entity.position.y + random()));
-        // entity.position.facing = Math.floor(Math.random() * 4) * 90;
+        entity.position.x = Math.max(0, Math.min(19, entity.position.x + random()));
+        entity.position.y = Math.max(0, Math.min(19, entity.position.y + random()));
+        entity.position.facing = Math.floor(Math.random() * 4) * 90;
     }
 
     // entity.position.x = 10;
--- battle-arena-react/src/lib/v2/manager/PlayerManager.js
+++ battle-arena-react/src/lib/v2/manager/PlayerManager.js
@@ -1,31 +1,7 @@
 import Agency from "@lespantsfancy/agency";
 
 import Registry from "./../util/Registry";
-import Watcher from "./../util/Watcher";
 
-// export class PlayerManager extends Watcher {
-//     constructor(players = []) {
-//         super([], {}, { deep: false });
-
-//         this.players = new Registry();
-
-//         this.add(...players);
-//     }
-
-//     add(...players) {
-//         for(let i = 0; i < players.length; i++) {
-//             const player = players[ i ];
-            
-//             if(!this.players.size && i === 0) {
-//                 this.players.register(player, "player");
-//             } else {
-//                 this.players.register(player);
-//             }
-
-//             player.$.subscribe(this);
-//         }
-//     }
-// }
 export class PlayerManager extends Registry {
     constructor(players = []) {
         super({}, { deep: true });
