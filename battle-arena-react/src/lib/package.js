49439f390e127943de08fca71d97558f573c1579
LesPantsFancy <mjnrock@umich.edu>
2022-05-08 22:27:12 -0400

    init

--- battle-arena-react/src/lib/package.js
+++ battle-arena-react/src/lib/package.js
@@ -37,23 +37,6 @@ export function CreateGame() {
 	SystemPosition.$("move", 2, 2, 2, true)([ node ]);		// === SystemPosition.Instances.get(`default`).trigger("move", node, 2, 2, 2, false);	
 	console.log(node.position.pos);
 
-	console.log(ComponentPosition.Create(world, {
-		x: () => Math.random(),
-		y: () => Math.random(),
-		z: () => Math.random(),
-	}, { evaluateState: true }));
-	console.log(ComponentPosition.Factory(1, [ world, {
-		x: () => Math.random(),
-		y: () => Math.random(),
-		z: () => Math.random(),
-	}, { evaluateState: true } ]));
-	// console.log(node.position.posObj);
-
-	// console.log(universe);
-	// console.log(world);
-	// console.log(world.physics.x);
-	// universe.trigger("join", Date.now())
-
     return game;
 }
 
