f82846d11bb04901f54aff7f371a7faf793e2f5b
LesPantsFancy <mjnrock@umich.edu>
2022-05-29 14:28:45 -0400

    init

--- battle-arena-react/src/lib/data/ecs/struct/Physics.js
+++ battle-arena-react/src/lib/data/ecs/struct/Physics.js
@@ -3,17 +3,17 @@ import { Bodies } from "matter-js";
 
 export class Physics extends Struct {
 	static Nomen = "physics";
-	
-	constructor(state = {}, opts = {}) {
+
+	constructor (state = {}, opts = {}) {
 		super({
 			world: null,
-			
+
 			body: Bodies.circle(0, 0, 1),
 			speed: 2.5,
 			range: 10,
-
-			...state,
 		}, opts);
+
+		this.upsert(state);
 	}
 };
 
