9d14c28f5515df09805bfd6dd44b0d2d9caeedd0
LesPantsFancy <mjnrock@umich.edu>
2022-06-05 18:54:09 -0400

    init

--- battle-arena-react/src/lib/@agency/core/EventList.js
+++ battle-arena-react/src/lib/@agency/core/EventList.js
@@ -1,61 +1,118 @@
-import { singleOrArrayArgs, spreadFirstElementOrArray } from "../util/helper";
+import { spreadFirstElementOrArray } from "../util/helper";
 
 export class EventList extends AgencyBase {
-	constructor() {
-		super();
+	constructor(events, aliases = [], agencyBaseObj = {}) {
+		super(agencyBaseObj);
 
+		/**
+		 * Hold a pre-defined list of events and their handlers.
+		 */
 		this.events = new Map();
+
+		/**
+		 * Hold a pre-defined list of events and their alias.
+		 */
 		this.aliases = new Map();
+
+		this.addEvent(events);
+		this.addAlias(aliases);
 	}
 
-	addHandler(event, handler) {
-		if(!this.events.has(event)) {
-			this.events.set(event, new Set());
-		}
-
-		this.events.get(event).add(handler);
-
-		return this;
-	}
-	addHandlers(event, ...handlers) {
+	//#region Event And Alias Manipulation
+	addEvent(event, ...handlers) {
 		handlers = spreadFirstElementOrArray(handlers);
 
 		if(typeof event === "object") {
 			const entries = Array.isArray(event) ? event : Object.entries(event);
 			for(const [ event, handler ] of entries) {
-				this.addHandler(event, handler);
+				this.addEvent(event, handler);
 			}
 		} else {
 			for(const handler of handlers) {
-				this.addHandler(event, handler);
+				if(!this.events.has(event)) {
+					this.events.set(event, new Set());
+				}
+		
+				this.events.get(event).add(handler);
 			}
 		}
 
 		return this;
 	}
-	removeHandler(event, handler) {
-		if(this.events.has(event)) {
-			return this.events.get(event).delete(handler);
-		}
-
-		return false;
-	}
-	removeHandlers(event, ...handlers) {
+	removeEvent(event, ...handlers) {
 		handlers = spreadFirstElementOrArray(handlers);
 
 		if(typeof event === "object") {
 			const entries = Array.isArray(event) ? event : Object.entries(event);
 			for(const [ event, handler ] of entries) {
-				this.removeHandler(event, handler);
+				this.removeEvent(event, handler);
 			}
 		} else {
 			for(const handler of handlers) {
-				this.removeHandler(event, handler);
+				if(this.events.has(event)) {
+					return this.events.get(event).delete(handler);
+				}
 			}
 		}
 
 		return this;
 	}
+
+	addAlias(event, aliases) {
+		if(typeof event === "object") {
+			this.addAliasObject(event);
+		} else {
+			for(const alias of aliases) {
+				this.aliases.set(event, alias);
+			}
+		}
+
+		return this;
+	}
+	addAliasObject(obj) {
+		const entries = Array.isArray(obj) ? obj : Object.entries(obj);
+
+		for(const [ event, ...aliases ] of entries) {
+			this.addAlias(event, ...aliases);
+		}
+
+		return this;
+	}
+	removeAlias(...events) {
+		for(const event of events) {
+			if(!this.aliases.delete(event)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	//#endregion Event And Alias Manipulation
+
+	//#region Agent Attachment
+	attach(agent) {
+		for(const [ event, handlers ] of this.events) {
+			for(const handler of handlers) {
+				const alias = this.aliases.get(event) || event;
+
+				agent.addEvent(alias, handler);
+			}
+		}
+
+		return this;
+	}
+	detach(agent) {
+		for(const [ event, handlers ] of this.events) {
+			for(const handler of handlers) {
+				const alias = this.aliases.get(event) || event;
+
+				agent.removeEvent(alias, handler);
+			}
+		}
+
+		return this;
+	}
+	//#endregion Agent Attachment
 };
 
 export default EventList;
