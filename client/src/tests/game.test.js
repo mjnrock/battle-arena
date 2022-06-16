a65a2827427d92f46d066724bb42cca2d7229f5b
LesPantsFancy <mjnrock@umich.edu>
2022-06-16 13:45:29 -0400

    init

--- client/src/tests/game.test.js
+++ client/src/tests/game.test.js
@@ -8,25 +8,25 @@ const game = new Game();
 // console.log(game);
 
 //? Verify that generators are working
-console.log(game.Environment.Generators.Entities.Squirrel.regenerateMany(3, {
-	Position: {
-		x: 5.12312,
-		y: 35.5,
-	},
-}));
-console.log(game.Environment.Generators.Entities.Squirrel.regenerate({
-	Position: {
-		x: 5.12312,
-		y: 35.5,
-	},
-}));
-// console.log(game.Environment.Generators.Entities.Squirrel.create().__entries);
-// console.log(game.Environment.Generators.Components.Position.create({ x: 5, y: 3 }));	// Args go to the data.components.Position(x, y)
-// console.log(game.Environment.Generators.Systems.Portal.create());
+// console.log(game.Environment.Generators.Entities.Squirrel.regenerateMany(3, {
+// 	Position: {
+// 		x: 5.12312,
+// 		y: 35.5,
+// 	},
+// }));
+// console.log(game.Environment.Generators.Entities.Squirrel.regenerate({
+// 	Position: {
+// 		x: 5.12312,
+// 		y: 35.5,
+// 	},
+// }));
+console.log(game.Environment.Generators.Entities.Squirrel.regenerate());
+// console.log(game.Environment.Generators.Components.Position.regenerate({ x: 5, y: 3 }));	// Args go to the data.components.Position(x, y)
+// console.log(game.Environment.Generators.Systems.Portal.regenerate());
 
 //? Verify that the tagging and pool results are working
-// const ent = game.Environment.Generators.Entities.Squirrel.create();
-// const ent2 = game.Environment.Generators.Entities.Squirrel.create();
+// const ent = game.Environment.Generators.Entities.Squirrel.regenerate();
+// const ent2 = game.Environment.Generators.Entities.Squirrel.regenerate();
 // ent.tags.add("squirrel");
 // ent2.tags.add("squirrel");
 // ent.tags.add("moo");
