1763b75c4e3349665192bb3b7c9431875cf04b31
LesPantsFancy <mjnrock@umich.edu>
2022-07-11 15:33:53 -0400

    init

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,8 +1,17 @@
 import Identity from "./lib/Identity";
 
+import Registry from "./lib/Registry";
+import { World } from "./data/entities/realm/World";
+
 export class Game extends Identity {
 	constructor({ id, tags } = {}) {
 		super({ id, tags });
+
+		this.realm = new Registry({
+			overworld: new World({
+				size: [ 10, 10 ],
+			}),
+		});
 
 		/**
 		 * Invoke all the creation hooks
