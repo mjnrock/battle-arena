2d9fcb1545bad4f1749713895586246ebf2b656b
LesPantsFancy <mjnrock@umich.edu>
2022-07-18 16:49:38 -0400

    init

--- client/src/routes/Default.jsx
+++ client/src/routes/Default.jsx
@@ -3,6 +3,10 @@ import { useEffect, useContext } from "react";
 import { Context } from "./../App";
 
 import { PixiCanvas } from "../components/PixiCanvas";
+
+
+import * as PixiJS from "pixi.js";
+// import testPixiMatter from "../PixiMatterTest";
 import Pixi from "../game/Pixi";
 
 const pixi = new Pixi({
@@ -14,16 +18,16 @@ export function Default() {
 	const { game } = useContext(Context);
 
 	//* This is a debugging invocation of the renderer, and should be handled by MainLoop
-	// useEffect(() => {
-	// 	const ticker = new PixiJS.Ticker();
-	// 	console.log(ticker)
-	// 	ticker.add(pixi.render.bind(pixi));
-	// 	ticker.start();
+	useEffect(() => {
+		const ticker = new PixiJS.Ticker();
+		console.log(ticker)
+		ticker.add(pixi.render.bind(pixi));
+		ticker.start();
 
-	// 	return () => {
-	// 		ticker.stop();
-	// 	};
-	// }, []);
+		return () => {
+			ticker.stop();
+		};
+	}, []);
 	
 	return (
 		// <PixiCanvas app={ game.render.app } />
