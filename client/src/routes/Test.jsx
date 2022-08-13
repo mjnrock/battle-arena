98bea8ea53fc6411c72d40592b7ca2f9e07d2410
LesPantsFancy <mjnrock@umich.edu>
2022-08-12 21:52:21 -0400

    init

--- client/src/routes/Test.jsx
+++ client/src/routes/Test.jsx
@@ -1,11 +1,32 @@
-import { Ticker } from "./../game/util/Ticker";
+import { Node } from "./../game/util/node/Node";
+import { Network } from "./../game/util/node/Network";
 
-// const loop = new Ticker({
-// 	fps: 5,
-// 	tick: ({ dt, now }) => console.log(dt),
-// });
+const n01 = new Node({
+	type: Node.EnumType.NAMESPACE,
+	state: "leaf",
+	events: {
+		pulse: () => console.log("MOOOOSE"),
+	},
+});
+const n1 = new Node();
+const n0 = new Node({
+	type: Node.EnumType.NAMESPACE,
+	state: "root",
+	events: {
+		pulse: () => console.log("MEOWWW"),
+	},
+	children: [
+		n01,
+	],
+});
 
-// loop.start();
+n01.events.on("pulse", (...args) => console.log("FIRED", args));
+n0.run(123, 123, 123, 123);
+console.log(n0);
+
+const network = new Network();
+network.add("circuit", n0);
+network.run("circuit", Date.now());
 
 export function Test() {
 	return (
