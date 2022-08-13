d76484a8c5ca88f41dd87f24b24292842f920f9c
LesPantsFancy <mjnrock@umich.edu>
2022-08-12 21:54:35 -0400

    init

--- client/src/game/util/relay/package.js
+++ client/src/game/util/relay/package.js
@@ -1,7 +1,11 @@
 import Runner from "./Runner";
 import GroupRunner from "./GroupRunner";
+import Invoker from "./Invoker";
+import Events from "./Events";
 
 export default {
 	Runner,
 	GroupRunner,
+	Invoker,
+	Events,
 };
