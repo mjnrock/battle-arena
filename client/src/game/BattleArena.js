236ba038ef631b457b15b17eebba012defd7656e
LesPantsFancy <mjnrock@umich.edu>
2022-08-20 08:36:35 -0400

    init

--- client/src/game/BattleArena.js
+++ client/src/game/BattleArena.js
@@ -54,77 +54,66 @@ export function loadInputControllers(game, { mouse, key } = {}) {
 
 export function createLayerTerrain(game) {
 	function drawEdgeMask(graphics, node) {
-		let [ tw, th ] = [ game.config.tile.width, game.config.tile.height ],
-		[ w, h ] = [ 15, 15 ];
+		let passes = [
+			[ 15, 15 ],
+			[ 25, 25 ],
+			[ 35, 35 ],
+		];
 
-		//* Begin the N, S, E, W edges
-		graphics.beginFill(0x000000, 0.1);
+		let i = 0;
+		for(let [ w, h ] of passes) {
+			let [ tw, th ] = [ game.config.tile.width, game.config.tile.height ];
 
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.LEFT)) {
-			graphics.drawRect(node.world.x * tw - w / 2, node.world.y * th, w, th);
+			//* Begin the N, S, E, W edges
+			graphics.beginFill(0x000000, 0.1 / (i + 1));
+			// graphics.beginFill(0xFF0000, 0.5 / (i + 1));
+
+			if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.LEFT)) {
+				graphics.drawRect(node.world.x * tw - w / 2, node.world.y * th, w, th);
+			}
+
+			if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.RIGHT)) {
+				graphics.drawRect(node.world.x * tw + tw - w + w / 2, node.world.y * th, w, th);
+			}
+
+			if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP)) {
+				graphics.drawRect(node.world.x * tw, node.world.y * th - h / 2, tw, h);
+			}
+
+			if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM)) {
+				graphics.drawRect(node.world.x * tw, node.world.y * th + th - h + h / 2, tw, h);
+			}
+
+			graphics.endFill();
+
+			//* Begin the NW, NE, SE, SW edges
+			// graphics.beginFill(0x0000FF, 0.5);
+	
+			// let radius = 10;
+			// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP_LEFT)) {
+			// 	graphics.drawRect(node.world.x * tw, node.world.y * th, radius, radius);
+			// 	// graphics.drawCircle(node.world.x * tw + radius, node.world.y * th + radius, radius);
+			// }
+	
+			// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP_RIGHT)) {
+			// 	graphics.drawRect(node.world.x * tw + tw - radius, node.world.y * th, radius, radius);
+			// 	// graphics.drawCircle(node.world.x * tw + tw - radius, node.world.y * th + radius, radius);
+			// }
+	
+			// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM_LEFT)) {
+			// 	graphics.drawRect(node.world.x * tw, node.world.y * th + th - radius, radius, radius);
+			// 	// graphics.drawCircle(node.world.x * tw + radius, node.world.y * th + th - radius, radius);
+			// }
+	
+			// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM_RIGHT)) {
+			// 	graphics.drawRect(node.world.x * tw + tw - radius, node.world.y * th + th - radius, radius, radius);
+			// 	// graphics.drawCircle(node.world.x * tw + tw - radius, node.world.y * th + th - radius, radius);
+			// }
+	
+			// graphics.endFill();
+
+			++i;
 		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.RIGHT)) {
-			graphics.drawRect(node.world.x * tw + tw - w + w / 2, node.world.y * th, w, th);
-		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP)) {
-			graphics.drawRect(node.world.x * tw, node.world.y * th - h / 2, tw, h);
-		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM)) {
-			graphics.drawRect(node.world.x * tw, node.world.y * th + th - h + h / 2, tw, h);
-		}
-
-		graphics.endFill();
-		[ w, h ] = [ 25, 25 ];
-
-		//* Double-pass the N, S, E, W edges with bigger, but lighter strokes for blending
-		graphics.beginFill(0x000000, 0.05);
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.LEFT)) {
-			graphics.drawRect(node.world.x * tw - w / 2, node.world.y * th, w, th);
-		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.RIGHT)) {
-			graphics.drawRect(node.world.x * tw + tw - w + w / 2, node.world.y * th, w, th);
-		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP)) {
-			graphics.drawRect(node.world.x * tw, node.world.y * th - h / 2, tw, h);
-		}
-
-		if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM)) {
-			graphics.drawRect(node.world.x * tw, node.world.y * th + th - h + h / 2, tw, h);
-		}
-
-		graphics.endFill();
-
-		//* Begin the NW, NE, SE, SW edges
-		// graphics.beginFill(0x0000FF, 0.5);
-
-		// let radius = 10;
-		// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP_LEFT)) {
-		// 	graphics.drawRect(node.world.x * tw, node.world.y * th, radius, radius);
-		// 	// graphics.drawCircle(node.world.x * tw + radius, node.world.y * th + radius, radius);
-		// }
-
-		// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.TOP_RIGHT)) {
-		// 	graphics.drawRect(node.world.x * tw + tw - radius, node.world.y * th, radius, radius);
-		// 	// graphics.drawCircle(node.world.x * tw + tw - radius, node.world.y * th + radius, radius);
-		// }
-
-		// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM_LEFT)) {
-		// 	graphics.drawRect(node.world.x * tw, node.world.y * th + th - radius, radius, radius);
-		// 	// graphics.drawCircle(node.world.x * tw + radius, node.world.y * th + th - radius, radius);
-		// }
-
-		// if(Bitwise.has(node.terrain.edges, EnumEdgeFlag.BOTTOM_RIGHT)) {
-		// 	graphics.drawRect(node.world.x * tw + tw - radius, node.world.y * th + th - radius, radius, radius);
-		// 	// graphics.drawCircle(node.world.x * tw + tw - radius, node.world.y * th + th - radius, radius);
-		// }
-
-		// graphics.endFill();
 	}
 
 	const layer = new Layer({
